5d3e26240b0d2fde1f3c344168f6ed4bbd46164f
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2023-07-28 19:12:42 +0500

    28.07.23

---
+++ client/src/app/web/avaialable-courses/avaialable-courses.component.ts
@@ -0,0 +1,29 @@
+import { Component,Input } from '@angular/core';
+import { ApiUtilsService } from '../../api-utils.service';
+
+
+@Component({
+  selector: 'app-avaialable-courses',
+  templateUrl: './avaialable-courses.component.html',
+  styleUrls: ['./avaialable-courses.component.css']
+})
+export class AvaialableCoursesComponent {
+  @Input() id :any;
+  isChecked: boolean = false;
+  constructor(private apiUtil:ApiUtilsService){
+  
+  }
+
+  checked(){
+    if(this.isChecked){
+      this.apiUtil.selectedCourse.push(this.id);
+
+    }
+    else {
+      this.apiUtil.selectedCourse = this.apiUtil.selectedCourse.filter(item => item !== this.id);
+
+    }
+  }
+  
+
+}
