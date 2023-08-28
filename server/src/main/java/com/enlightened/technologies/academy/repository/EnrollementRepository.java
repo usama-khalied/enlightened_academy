aee62d1d99f4d5cc313ad9a1c46e5cfacd3952cc
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2023-08-28 11:38:58 +0500

    Add Method in Enrollment Repository For Specific Student

--- server/src/main/java/com/enlightened/technologies/academy/repository/EnrollementRepository.java
+++ server/src/main/java/com/enlightened/technologies/academy/repository/EnrollementRepository.java
@@ -1,12 +1,19 @@
 package com.enlightened.technologies.academy.repository;
 
+import com.enlightened.technologies.academy.model.Student;
 import com.enlightened.technologies.academy.model.Enrollment;
+
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+
 
 /**
  *
  * @author hasan
  */
 public interface EnrollementRepository extends JpaRepository<Enrollment, String>{
-    
+
+    List<Enrollment> findByStudentId(@Param("studentId") String studentId);
 }
