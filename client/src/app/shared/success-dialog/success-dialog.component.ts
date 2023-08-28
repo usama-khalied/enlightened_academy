192808016b6e0dca92eaf7c780f463b7d1c1efb7
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2023-08-29 01:20:58 +0500

    Add Generate button in success modal for generating voucher

--- client/src/app/shared/success-dialog/success-dialog.component.ts
+++ client/src/app/shared/success-dialog/success-dialog.component.ts
@@ -2,6 +2,10 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { animate, style, transition, trigger } from '@angular/animations';
 import { VoucherService } from '../service/voucher.service';
+import { Student } from '../model/Student';
+import { HttpResponse } from '../model/HttpResponse';
+import { Enrollment } from '../model/Enrollment';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-success-dialog',
@@ -20,12 +24,38 @@ import { VoucherService } from '../service/voucher.service';
   ]
 })
 export class SuccessDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { message: string ,downloadPdf?:string},
-  public voucherService:VoucherService,
-  private dialogRef: MatDialogRef<SuccessDialogComponent>) {}
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { message: string, downloadPdf?: string, studentId: any },
+    public voucherService: VoucherService,
+    private dialogRef: MatDialogRef<SuccessDialogComponent>, private router: Router,) { }
 
-  closeDialog() {
-    this.dialogRef.close();
+ 
+  generatePdf(): void {
+    this.voucherService.getStudentAndCourseListById(this.data?.studentId).subscribe({
+      next: (response: HttpResponse) => {
+        const student = response.data as Student;
+        if (student) {
+          this.voucherService.student = [student];
+          this.voucherService.courseList = student.enrollments as unknown as Enrollment[];
+          this.voucherService.calculateTotalFee();
+          this.voucherService.runReport('Enlightened Academy');
+        } else {
+          console.error('Student data not found.');
+        }
+      },
+      error: (error) => {
+        console.error('API error:', error);
+      }
+    });
   }
+
+
+  // generatePdf():void {
+  //   this.voucherService.getStudentAndCourseListById(this.data?.studentId).subscribe((response:HttpResponse) => {
+  //     const student = response.data as Student;
+  //     this.voucherService.student = [student];
+  //     this.voucherService.courseList = student.enrollments as unknown as Enrollment[];
+  //   })
+
+  // }
 
 }
