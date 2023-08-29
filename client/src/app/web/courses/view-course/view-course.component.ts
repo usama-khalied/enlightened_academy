d64b336871684330bb31349b0e78384c7e42d109
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2023-08-30 01:11:02 +0500

    Change Name

---
+++ client/src/app/web/courses/view-course/view-course.component.ts
@@ -0,0 +1,14 @@
+import { Component,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-view-course',
+  templateUrl: './view-course.component.html',
+  styleUrls: ['./view-course.component.css']
+})
+export class ViewCourseComponent {
+  // @Input() courseBackground:any;
+  // @Input() courseDescription:any;
+  // @Input() courseName:any;
+  // @Input() courseFee:any;
+
+}
