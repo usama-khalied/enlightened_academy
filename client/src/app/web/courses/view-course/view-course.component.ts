b78ee21b7bac15af05a86cb699084197e215068c
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2023-09-01 00:47:15 +0500

    View Courses Page

--- client/src/app/web/courses/view-course/view-course.component.ts
+++ client/src/app/web/courses/view-course/view-course.component.ts
@@ -1,9 +1,32 @@
-import { Component,Input } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from 'src/app/shared/model/Course';
+import { CourseService } from 'src/app/shared/service/course.service';
 
 @Component({
   selector: 'app-view-course',
   templateUrl: './view-course.component.html',
-  styleUrls: ['./view-course.component.css']
+  styleUrls: ['./view-course.component.scss']
 })
-export class ViewCourseComponent {
+export class ViewCourseComponent implements OnInit {
+  id: string | number = -1;
+  loading: boolean = false;
+  courseList:Course[] = [];
+
+  constructor(private route :ActivatedRoute,private courseService: CourseService){
+    
+  }
+  ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+    this.getCourse();
+  }
+  getCourse():void {
+    this.courseService.getCourse(this.id).subscribe({
+      next: (response) => {
+        this.loading = false;
+        this.courseList.push(response.data as unknown as Course)
+      },
+      error: (error) => console.error('Failed to fetch courses:', error),
+    });
+  }
 }
